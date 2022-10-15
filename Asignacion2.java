710b8e287b683d82ad1d6be6e656c205422978fb
Rangel Alvarado <rangelalvarado1@gmail.com>
2022-10-15 10:06:20 -0500

    Asking the user for the inputs

---
+++ Asignacion2.java
@@ -0,0 +1,17 @@
+/**
+ * Asignacion 2
+ */
+
+public class Asignacion2 {
+  private final double velocidadLuz = 299792.458;
+  private final double velocidadSonido = 0.331;
+
+  public static void main(String[] args) {
+    System.out.println("Inserte el tiempo prolongado X");
+    double X = Integer.parseInt(System.console().readLine());
+    System.out.println("Inserte el tiempo Y");
+    double Y = Integer.parseInt(System.console().readLine());
+    System.out.println("Inserte el tiempo Z");
+    double Z = Integer.parseInt(System.console().readLine());
+  }
+}
