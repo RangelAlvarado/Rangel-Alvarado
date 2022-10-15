e39fdd40fc9e835cbc56d21284bfe9173e0e4a0b
Rangel Alvarado <rangelalvarado1@gmail.com>
2022-10-15 10:24:52 -0500

    Main code writing

--- Asignacion2.java
+++ Asignacion2.java
@@ -3,15 +3,36 @@
  */
 
 public class Asignacion2 {
-  private final double velocidadLuz = 299792.458;
-  private final double velocidadSonido = 0.331;
 
   public static void main(String[] args) {
+    double velocidadLuz = 299792.458;
+    double velocidadSonido = 0.331;
     System.out.println("Inserte el tiempo prolongado X");
-    double X = Integer.parseInt(System.console().readLine());
+    double x = Integer.parseInt(System.console().readLine());
     System.out.println("Inserte el tiempo Y");
-    double Y = Integer.parseInt(System.console().readLine());
+    double y = Integer.parseInt(System.console().readLine());
     System.out.println("Inserte el tiempo Z");
-    double Z = Integer.parseInt(System.console().readLine());
+    double z = Integer.parseInt(System.console().readLine());
+
+    double desaceleracion = Desaceleracion(x, velocidadLuz);
+    double kmRecorridos = KmRecorridos(desaceleracion, z, velocidadSonido);
+
+    System.out.println("La desaceleracion es de " + desaceleracion);
+    System.out.println("Kilometros recorridos fue de " + kmRecorridos);
+  }
+
+  private static double KmRecorridos(
+    double desaceleracion,
+    double z,
+    double velocidadSonido
+  ) {
+    double velocidadFinal = velocidadSonido + desaceleracion * z;
+    double distancia = velocidadFinal * z;
+    return distancia;
+  }
+
+  private static double Desaceleracion(double x, double velocidadLuz) {
+    double desaceleracion = 0 - velocidadLuz / x;
+    return desaceleracion;
   }
 }
